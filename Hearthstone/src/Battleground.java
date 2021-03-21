1a2d3f8b0f9ccf5a4370d2301e2447da128fd0e1
Luna Wilkes <luna.wilkes@gmail.com>
2021-03-22 12:37:23 +1300

    Added more flexibility to the creation of boards

--- Hearthstone/src/Battleground.java
+++ Hearthstone/src/Battleground.java
@@ -8,49 +8,77 @@ import java.util.Scanner;
 public class Battleground
 {
     public static void main(String[] args) {
-        Board topBoard = new Board("Top");
-        Board lowerBoard = new Board("Lower");
+        Board topBoard; // = new Board("Top", 7);
+        Board lowerBoard; // = new Board("Lower", 7);
         int first;
         Scanner input = new Scanner(System.in);
         String stringInput;
+        int intInput = 0;
+        boolean askAgain = true;
         
+        // Initialize top board
+        while (askAgain) {
+        	System.out.println("How many cards does the top board have? ");
+        	if (input.hasNextInt()) {
+        		intInput = input.nextInt();
+        		askAgain = false;
+        	}
+        	else {
+        		System.out.println("Please enter a number");
+        	}
+        }
+        topBoard = new Board("Top", intInput);
+        
+        // Initialize lower board
+        askAgain = true;
+        while (askAgain) {
+        	System.out.println("How many cards does the lower board have? ");
+        	if (input.hasNextInt()) {
+        		intInput = input.nextInt();
+        		askAgain = false;
+        	}
+        	else {
+        		System.out.println("Please enter a number");
+        	}
+        }
+        lowerBoard = new Board("Lower", intInput);
         // Show both boards
         System.out.println("Top board:");
         topBoard.displayBoard();
         
         System.out.println("\nLower board:");
         lowerBoard.displayBoard();
-        try {
-	        do {
-	        	System.out.println("Press enter to continue");
-	        	stringInput = input.nextLine();
-	        } while (!stringInput.equals(""));
-        } finally {
-        	input.close();
-        }
+        
+	    do {
+	       	System.out.println("Press enter to continue");
+	       	input.nextLine();
+	        stringInput = input.nextLine();
+	    } while (!stringInput.equals(""));
+        input.close();
         
         do { // run game while both boards have cards on them
             first = (int) (Math.random());
             
             if (first == 0) { // top board goes first
             	// cycle through, starting on 1. odds is first evens are second.
-                for (int i = 0; i < topBoard.getMax(); i++) {
+                for (int i = 0; i < topBoard.getLen(); i++) {
                 	// Check that the card is alive
                     if (topBoard.getDeck()[i].isAlive()) {
-                        topBoard.randomAttack(i, lowerBoard.getDeck());
+                        topBoard.randomAttack(i, lowerBoard);
                     }
                     if (lowerBoard.getDeck()[i].isAlive()) {
-                        lowerBoard.randomAttack(i, topBoard.getDeck());
+                        lowerBoard.randomAttack(i, topBoard);
                     }
                 }
             }
             else if (first == 1) { // bottom board goes first
-                for (int i = 0; i < topBoard.getMax(); i++) {
-                    if (lowerBoard.getDeck()[i].isAlive()) { // Check that the card is alive
-                        lowerBoard.randomAttack(i, topBoard.getDeck());
+                for (int i = 0; i < topBoard.getLen(); i++) {
+                	// Check that the card is alive
+                    if (lowerBoard.getDeck()[i].isAlive()) {
+                        lowerBoard.randomAttack(i, topBoard);
                     }
                     if (topBoard.getDeck()[i].isAlive()) {
-                        topBoard.randomAttack(i, lowerBoard.getDeck());
+                        topBoard.randomAttack(i, lowerBoard);
                     }
                 }
             }
@@ -68,6 +96,5 @@ public class Battleground
         else {
         	System.out.println("nothing");
         }
-        System.out.println("??");
     }
 }
--- Hearthstone/src/Board.java
+++ Hearthstone/src/Board.java
@@ -6,24 +6,33 @@
  */
 public class Board
 {
-    private final int MIN = 1, MAX_CARDS = 7;
+    private final int MIN = 1;
     
     // Array for the cards
-    private Card[] cards = new Card[MAX_CARDS];
+    private Card[] cards;
     
     /**
      * Constructor for objects of class Board
      */
-    public Board(String boardName) {
-        this.fillBoard(boardName);
+    public Board(String boardName, int cardAmt) {
+        this.fillBoard(boardName, cardAmt);
     }
     
-    public void fillBoard(String boardName) {
-        for (int i = 0; i < MAX_CARDS; i++) {
+    /**
+     * Fills the board with cards
+     * @param boardName name of the board
+     * @param cardsAmt amount of cards on the board
+     */
+    public void fillBoard(String boardName, int cardsAmt) {
+    	cards = new Card[cardsAmt];
+        for (int i = 0; i < cardsAmt; i++) {
             cards[i] = new Card(boardName + " Card " + Integer.toString(i + 1));
         }
     }
     
+    /**
+     * Prints out all the cards on the board
+     */
     public void displayBoard() {
     	for (Card card: cards) {
     		System.out.printf("%s. Attack: %s. Defence: %s\n", card.getName(),
@@ -31,19 +40,23 @@ public class Board
     	}
     }
     
-    public void randomAttack(int idx, Card[] defenders) {
+    public void randomAttack(int idx, Board defenders) {
         int random;
         do {
-            random = (int) (Math.random() * MAX_CARDS);
-        } while (!defenders[random].isAlive());
-        defenders[random].getHit(cards[idx].getAttack());
-        cards[idx].getHit(defenders[random].getAttack());
-        System.out.printf("\n%s attacks %s!", cards[idx].getName(), defenders[random].getName());
-        if (defenders[random].getDefence() <= 0) {
-            System.out.printf("\n%s is dead :(", defenders[random].getName());
-        }
-        if (cards[idx].getDefence() <= 0) {
-            System.out.printf("\n%s is dead :(", cards[idx].getName());
+            random = (int) (Math.random() * defenders.getDeck().length);
+        } while ((!defenders.getCard(random).isAlive())
+        	&& (defenders.checkDeck() != 0));
+        if (defenders.checkDeck() != 0) {
+	        defenders.getCard(random).getHit(cards[idx].getAttack());
+	        cards[idx].getHit(defenders.getCard(random).getAttack());
+	        System.out.printf("\n%s attacks %s!", cards[idx].getName(),
+	        		defenders.getCard(random).getName());
+	        if (defenders.getCard(random).getDefence() <= 0) {
+	            System.out.printf("\n%s is dead :(", defenders.getCard(random).getName());
+	        }
+	        if (cards[idx].getDefence() <= 0) {
+	            System.out.printf("\n%s is dead :(", cards[idx].getName());
+	        }
         }
     }
     
@@ -63,11 +76,15 @@ public class Board
         return cards;
     }
     
-    public int getRandom() {
-        return (int) (MIN + Math.random() * MAX_CARDS);
+    public Card getCard(int idx) {
+    	return cards[idx];
     }
     
-    public final int getMax() {
-        return MAX_CARDS;
+    public int getRandom() {
+        return (int) (MIN + Math.random() * cards.length);
+    }
+    
+    public final int getLen() {
+        return cards.length;
     }
 }
